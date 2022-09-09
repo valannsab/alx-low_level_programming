fe62888c865768b08e81a656a8a2002cd7b5089d
valannsab <valerieannsabala2010@gmail.com>
2022-09-09 06:16:46 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/* code that prints exactly*/
 int main(void)
 { 
-	puts("programming is like building a multilingual puzzle\n");
+	puts("programming is like building a multilingual puzzle");
 	return (0);
 }
