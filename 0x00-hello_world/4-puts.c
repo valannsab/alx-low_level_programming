441e4f05a77e57edc38d62d144a1efd782791ef7
valannsab <valerieannsabala2010@gmail.com>
2022-09-10 00:32:57 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * program that prints text exactly
- * followed by new line.
+ * Descriprtion: program that prints text exactly
+ * followed by new line
  */
 int main(void)
-{ 
+{
 	puts("\"programming is like building a multilingual puzzle\n");
 	return (0);
 }
