935c451928acc0d2932012cc8690557b851c3971
valannsab <valerieannsabala2010@gmail.com>
2022-09-10 09:47:49 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,18 +18,16 @@ int main(void)
 	{
 		for (digit2 = 0; digit2 < 10; digit2++)
 		{
-		putchar((digit1 % 10) + '0');
-		putchar((digit2 % 10) + '0');
-		
-	if (digit1 == 9 && digit2 == 9)
-		continue;
-		putchar(',');
-		putchar(' ');
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 9 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
 	putchar('\n');
 	return (0);
 }
-
-
