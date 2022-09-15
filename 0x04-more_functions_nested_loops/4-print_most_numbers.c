872683aec4667ec95aece85d73b048c30eb0f2d0
valannsab <valerieannsabala2010@gmail.com>
2022-09-15 15:02:31 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * followed by a new line
+ * Return: returns nothing
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if ((n == 50) || (n == 52))
+		{
+			continue;
+		}
+		putchar(n);
+	}
+	putchar(10);
+}
