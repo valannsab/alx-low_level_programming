d09bb89e954fd59730f3493d4d7b008c6548ad29
valannsab <valerieannsabala2010@gmail.com>
2022-09-15 14:42:39 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * followed by new line
+ * Return: 0
+ */
+int print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58, i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+}
