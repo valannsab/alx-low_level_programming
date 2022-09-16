56a5e8e47e57f5ad3ca523265f2f74a106a2c17a
valannsab <valerieannsabala2010@gmail.com>
2022-09-15 17:20:43 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,19 +14,18 @@ void print_triangle(int size)
 		{
 			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
-				putchar(' ');
+				_putchar('\n');
 			}
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
 			if (inc1 == size)
 			{
 				continue;
 			}
-			putchar('#');
+			_putchar('#');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
-	return (0);
 }
