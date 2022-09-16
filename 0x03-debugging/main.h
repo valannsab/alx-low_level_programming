230b3caaa5d79549b7e773f55b73429edb439df6
valannsab <valerieannsabala2010@gmail.com>
2022-09-15 18:22:05 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+
+void positive_or_negative(int 1);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H*/
