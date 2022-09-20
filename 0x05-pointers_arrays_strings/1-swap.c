320f16adaecf1892ea5c37670060e0093f023bb1
valannsab <valerieannsabala2010@gmail.com>
2022-09-20 09:57:35 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,17 @@
-#inc
+#include "main.h"
+
+/**
+ * swap_int - entry point
+ * Description: function that swaps the values of two integers
+ * @a:first integer
+ * @b: second integer
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
