fa8c09adbbfde5f84d10d483a41062a8488785ac
valannsab <valerieannsabala2010@gmail.com>
2022-09-20 09:51:08 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,7 @@
  * reset_to_98 - entry point
  * Description: program takes a pointer to an int as parameter
  * and updates the value it points to to 98
+ * @n: parameter
  * Return: nothing
  */
 
